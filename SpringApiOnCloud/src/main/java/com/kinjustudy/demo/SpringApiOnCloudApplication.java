6d5a271588e93c4a1512a89c7a91c31770b8e997
windows <windows@DESKTOP-KQN37IS>
2021-06-21 19:23:14 +0530

    First commit

---
+++ SpringApiOnCloud/src/main/java/com/kinjustudy/demo/SpringApiOnCloudApplication.java
@@ -0,0 +1,13 @@
+package com.kinjustudy.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiOnCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiOnCloudApplication.class, args);
+	}
+
+}
---
+++ SpringApiOnCloud/src/test/java/com/kinjustudy/demo/SpringApiOnCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kinjustudy.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiOnCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
